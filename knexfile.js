d160b51adb9625a6898ddb75b447ed8c10e33e27
CamiloFlezLadeCode <camilodesarrollador@outlook.com>
2025-10-05 10:57:45 -0500

    SE IMPLEMENTA CAPTURA DE ENTORNO DINAMICO PARA KNEX EJECUTAR OPERACIONES / MIGRACIONES Y DEMÁS

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,61 @@
+// // ORIGINAL INICIAL
+// require('dotenv').config();
+// // Update with your config settings.
+// /**
+//  * @type { Object.<string, import("knex").Knex.Config> }
+//  */
+// module.exports = {
+
+//   development: {
+//     client: 'mysql2',
+//     connection: {
+//       host: 'localhost',
+//       user: 'root',
+//       password: null,
+//       database: 'tecniservicios'
+//     },
+//     pool: { min: 2, max: 10 },
+//     migrations: {
+//       directory: './migrations',
+//       tableName: 'knex_migrations'
+//     },
+//     seeds: {
+//       directory: './seeds'
+//     }
+//   },
+
+//   production: {
+//     client: 'mysql2',
+//     connection: {
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME
+//     },
+//     pool: { min: 2, max: 10 },
+//     migrations: {
+//       directory: './migrations',
+//       tableName: 'knex_migrations'
+//     },
+//     seeds: {
+//       directory: './seeds'
+//     }
+//   }
+
+// };
+
+
+
+// NUEVA METODOLOGÍA PARA CAPTURAR DINAMICAMENTE EL ENTORNO A EJECUTAR
 require('dotenv').config();
 // Update with your config settings.
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
 
-  development: {
+const Environment = process.env.VARIABLE_ENV_ENTORNO;
+const config = {
+  Desarrollo: {
     client: 'mysql2',
     connection: {
       host: 'localhost',
@@ -23,7 +73,7 @@ module.exports = {
     }
   },
 
-  production: {
+  Produccion: {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST,
@@ -40,5 +90,6 @@ module.exports = {
       directory: './seeds'
     }
   }
-
-};
+}
+const EnvironmentConfig = config[Environment];
+module.exports = EnvironmentConfig;
